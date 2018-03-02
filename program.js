b505d40d8e2b0b0d27c64bcfd0c1c1eea21bae22
Andrew Rogachevich <andrey.rogachevich@azati.com>
2018-03-02 14:17:20 +0300

    HTTP JSON API SERVER

--- program.js
+++ program.js
@@ -1,11 +1,31 @@
 const http = require('http');
+const url = require('url');
 
 const map = require('through2-map');
 
 const server = http.createServer((request, response) => {
-	if (request.method === 'POST'){
-		request.pipe(map((chunk) => chunk.toString().toUpperCase())).pipe(response);
+	if (request.method !== 'GET') {
+		response.writeHead(404, { 'Content-Type': 'application/json' });
+		response.end();
 	}
+	let s = url.parse(request.url, true);
+	let obj = {};
+
+	if (s.pathname === '/api/parsetime') {
+		let data = new Date(s.query.iso);
+		obj = {
+			hour: data.getHours(),
+			minute: data.getMinutes(),
+			second: data.getSeconds()
+		};
+	} else {
+		let data = new Date(s.query.iso);
+		obj = {
+			unixtime: data.getTime()
+		};
+	}
+	response.writeHead(200, { 'Content-Type': 'application/json' });
+	response.end(JSON.stringify(obj));
 });
 
 server.listen(process.argv[2]);
