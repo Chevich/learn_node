c4015f368df4a1b0b6b5cced6a6d6e01ecd2b070
Andrew Rogachevich <andrey.rogachevich@azati.com>
2018-03-02 16:33:47 +0300

    HANDLING

--- program.js
+++ program.js
@@ -1,23 +1,32 @@
 const Hapi = require('hapi');
+const Inert = require('inert');
 
 const server = Hapi.Server({
 	host: 'localhost',
-	port: Number(process.argv[2] || 8080)
+	port: Number(process.argv[2] || 8080),
+	routes: {
+		files: {
+			relativeTo: __dirname
+		}
+	}
 });
-
-server.route({
-	path: '/{name}',
-	method: 'GET',
-	handler: (request, h) => `Hello ${encodeURIComponent(request.params.name)} `
-});
-
 
 (async () => {
 	try {
+		await server.register(Inert);
+
+		server.route({
+			path: '/',
+			method: 'GET',
+			handler: {
+				file: 'index.html'
+			}
+		});
+
 		await server.start();
 
-		console.log(`Server running at: ${server.info.uri}`);
 	} catch (error) {
 		console.log(error);
 	}
 })();
+
