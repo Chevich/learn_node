d9d69e0ff15db28b899a22f7f85d0abfb5a8070b
Andrew Rogachevich <andrey.rogachevich@azati.com>
2018-03-02 09:44:29 +0300

    HTTP COLLECT (1)

--- program.js
+++ program.js
@@ -1,5 +1,12 @@
 let http = require('http');
 
 http.get(process.argv[2], function(res) {
-	res.setEncoding('utf8').on("data", console.log).on('error', console.error);
+	let data = '';
+	res.setEncoding('utf8');
+	res.on("data", (answer) => data += answer);
+	res.on('end', () => {
+		console.log(data.length);
+		console.log(data);
+	});
+	res.on('error', console.error);
 }).on('error', console.error);
